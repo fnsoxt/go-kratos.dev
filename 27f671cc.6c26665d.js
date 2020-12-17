f2a5aab1765fc7b0fc07cd98ebdaf8846027badd
gh-actions <actions@gihub.com>
2020-12-17 16:59:17 +0000

    Deploy website - based on 518afcb9f2b19d51a28a669bd2615197cbbd94d0

---
+++ 27f671cc.6c26665d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{72:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hello World","permalink":"/blog/hello-world"}]}')}}]);
